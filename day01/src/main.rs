6b174649e306eb82015391919f2f161172c2da23
Mehmet Denizhan Erdem <denizhanerdem@gmail.com>
2019-12-01 13:16:45 +0300

    day1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
 fn main() {
-    println!("herro");
+    let reader = BufReader::new(File::open("input.txt").unwrap());
+    let sum: i32 = reader
+        .lines()
+        .map(|x| x.unwrap().parse::<i32>().unwrap())
+        .map(p2)
+        .sum();
+    println!("{}", sum);
+}
+
+fn p2(module: i32) -> i32 {
+    let v = p1(module);
+    if v <= 0 {
+        return 0;
+    }
+    v + p2(v)
+}
+
+fn p1(module: i32) -> i32 {
+    module / 3 - 2
 }
